6d939317c4c715f2a5a522fce16fe79ec336e850
aks1205 <130886914+aks1205@users.noreply.github.com>
2024-08-12 19:38:34 +0000

    Pending changes exported from your codespace

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class RewardsConverter {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
